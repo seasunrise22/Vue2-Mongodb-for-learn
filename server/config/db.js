6a0907113e1291df23c96814bf5011005eb0a2d6
seasunrise22 <seasunrise22@gmail.com>
2024-09-24 15:33:15 +0900

    db.js 파일에 db 설치관련 기록

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,12 @@
+/**
+ * 몽고db 설치는 Node.js 교과서 367p '몽고디비 설치하기'를 따랐음.
+ * 콤파스로 커넥션 생성할 때 URI는 'mongodb://lee:1234@localhost:27017/admin' 로 생성.
+ * 콤파스 커넥션 생성 시 Athentication Method 에서 username/password 에 각각 db.createUser로 생성했던 계정정보 입력해 줌.
+ * 커넥션 생성 후 서버 실행해도 'learnVue' DB는 자동으로 생성되지 않고, 실제 데이터를 넣으면 생성 됨.
+ * 경로 끝에 admin 넣는 이유는 lee 계정이 admin 데이터베이스에서 생성된 계정이기 때문에, 연결 시 admin 데이터베이스에서 인증을 받아야 함.
+ * admin을 지정해주지 않으면 기본적으로 test 데이터베이스로 연결되기 때문에 인증에 실패할 우려가 있음.
+ */
+
 import mongoose, { mongo } from "mongoose";
 import dotenv from 'dotenv';
 
